4da03d4eb2a43296fd9f826ed5d24c7e2e731114
Sébastien XAVIER CARLOS <sebastien.xaviercarlos@hotmail.com>
2019-12-03 21:29:55 +0100

    test down

--- public/secret.js
+++ public/secret.js
@@ -68,7 +68,7 @@ var Konami = function (callback) {
             capture: false,
             orig_keys: "",
             //keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP"],
-            keys: ["UP", "UP", "DOWN", "DOWN", "LEFT", "RIGHT", "LEFT", "RIGHT", "TAP", "TAP", "TAP"],
+            keys: ["UP", "UP", "DOWN", "DOWN", "TAP", "TAP", "TAP"],
             input: [],
             code: function (link) {
                 konami.code(link);
